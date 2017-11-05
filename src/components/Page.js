0ff6e809fb5d5e204ead43a0b91417b8322e2365
pleportz <pleportz@gmail.com>
2017-11-05 18:25:25 +0100

    Remove proptypes from Page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,33 +1,22 @@
 // @flow
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import appStyle from 'HowMany/src/appStyle';
 
 const Page = props => (
   <View
-    style={[styles.page, {
-      paddingTop: props.noNavBar ? 0 : appStyle.grid.x2,
-      paddingHorizontal: props.noMargin ? 0 : appStyle.grid.x3,
-      backgroundColor: props.backgroundColor,
-    }]}
+    style={[
+      styles.page,
+      {
+        paddingTop: props.noNavBar ? 0 : appStyle.grid.x2,
+        paddingHorizontal: props.noMargin ? 0 : appStyle.grid.x3,
+        backgroundColor: props.backgroundColor,
+      },
+    ]}
   >
     {props.children}
   </View>
 );
-
-Page.propTypes = {
-  children: PropTypes.node.isRequired,
-  noMargin: PropTypes.bool,
-  noNavBar: PropTypes.bool,
-  backgroundColor: PropTypes.string,
-};
-
-Page.defaultProps = {
-  children: null,
-  noMargin: false,
-  noNavBar: false,
-  backgroundColor: appStyle.colors.color,
-};
 
 const styles = StyleSheet.create({
   page: {
